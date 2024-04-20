ff162c968cf5e7fe5aed83d54c4d37addeaa4e77
unknown <PeterDiyo>
2024-04-20 16:39:40 +0530

    first commit

---
+++ tasks/src/App.jsx
@@ -0,0 +1,15 @@
+import { ToastContainer } from "react-toastify";
+import Form from "./Form";
+import Items from "./Items";
+
+const App = () => {
+  return (
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form />
+      <Items />
+    </section>
+  );
+};
+
+export default App;
